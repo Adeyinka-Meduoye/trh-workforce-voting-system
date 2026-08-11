441df005a0ecb363719f017ed5b1f7acb64e8e3e
Adeyinka-Meduoye <yinkopet@gmail.com>
2026-08-11 18:20:38 +0100

    july 2026 winner

--- services/data.ts
+++ services/data.ts
@@ -65,6 +65,13 @@ export const organisations: Organisation[] = [
 ];
 
 export const winners: Winner[] = [
+        {
+    name: "Deborah Aniebiet Ekanem",
+    org: "Sanctuary (VineDressers) Organisation",
+    month: "July",
+    year: 2026,
+    image: "/images/July2026.jpg"
+  },
       {
     name: "Nzedeh Marvelous Uche  & Henry Chinedu",
     org: "Music (WinePress) Organisation & Protocol (Ushering) Organisation",
